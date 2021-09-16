bd751d59621a79fab6b933a7ff4390891878be68
Ridepad <ridepad@mail.ru>
2021-09-16 11:22:44 +0300

    trying new library
    new keyboard listener library + better exit function

--- start.py
+++ start.py
@@ -1,8 +1,15 @@
-import keyboard
 import subprocess
+
+import pynput
 import win32clipboard
 
-#TODO: open new window to select server
+
+HOTKEY_NEW_WINDOW = '<ctrl>+<f1>'
+HOTKEY_EXIT = '<ctrl>+<f2>'
+WINDOWS = []
+win32clipboard.OpenClipboard()
+win32clipboard.EmptyClipboard()
+win32clipboard.CloseClipboard()
 
 def new_window():
     win32clipboard.OpenClipboard()
@@ -14,16 +21,23 @@ def new_window():
             w = subprocess.Popen(['main.exe', _clipboard])
         except FileNotFoundError: #developer debug version
             w = subprocess.Popen(['python', 'main.pyw', _clipboard])
-        windows.append(w)
+        print('LOG:   NEW WINDOW FOR:', _clipboard)
+        WINDOWS.append(w)
     except TypeError:
         print('ERROR: Clipboard is empty!')
         win32clipboard.CloseClipboard()
 
-windows = []
-win32clipboard.OpenClipboard()
-win32clipboard.EmptyClipboard()
-win32clipboard.CloseClipboard()
-keyboard.add_hotkey('ctrl+F1', new_window, suppress=True)
-keyboard.wait('ctrl+F2')
-for w in windows:
-    w.terminate()
+def _exit():
+    for w in WINDOWS:
+        subprocess.call(f'taskkill /pid {w.pid} /f /t')
+    exit()
+
+def main():
+    _keys = {
+        HOTKEY_NEW_WINDOW: new_window,
+        HOTKEY_EXIT: _exit,
+        }
+    with pynput.keyboard.GlobalHotKeys(_keys) as listener:
+        listener.join()
+
+main()
