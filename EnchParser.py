fdcbfc075c0630e5476907cbbc7576dc5474e89d
Ridepad <ridepad@mail.ru>
2022-01-27 10:14:17 +0200

    1.3

--- EnchParser.py
+++ EnchParser.py
@@ -1,9 +1,19 @@
+import json
 import re
-import requests
+import threading
 
-ench_url = "https://wotlk.evowow.com/?enchantment="
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+HEADERS = {'User-Agent': "EnchParser"}
+ENCH_CACHE = 'Ench_cache'
+
+CACHED: dict[str, dict] = {}
+LOADING: dict[str, threading.Thread] = {}
+
 BASE_STATS = {'stamina', 'intellect', 'spirit', 'strength', 'agility'}
-short_stats = {
+SHORT_STATS = {
     'armorpenrtng': 'armor penetration rating',
     'resirtng': 'resilience rating',
     'hitrtng': 'hit rating',
@@ -16,74 +26,115 @@ short_stats = {
     'dodgertng': 'dodge rating',
     'parryrtng': 'parry rating',
     'manargn': 'mp5',
+    'healthrgn': 'hp5',
     'sta': 'stamina',
     'int': 'intellect',
     'spi': 'spirit',
     'str': 'strength',
     'agi': 'agility',
-    'armor': 'armor'}
+}
 
-loaded = {}
-loading = set()
-def get_ench_raw(ench_ID):
-    if ench_ID in loaded:
-        return loaded[ench_ID]
-    elif ench_ID in loading:
-        while ench_ID in loading:
+def get_page(ench_ID):
+    url = f"https://wotlk.evowow.com/?enchantment={ench_ID}"
+    for _ in range(3):
+        try:
+            page = requests.get(url, headers=HEADERS, timeout=2, allow_redirects=False)
+            if page.status_code == 200:
+                return page.text
+        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
             pass
-        return loaded[ench_ID]
-    loading.add(ench_ID)
-    ench = loaded[ench_ID] = requests.get(f'{ench_url}{ench_ID}').text
-    loading.remove(ench_ID)
-    return ench
 
-def get_names(ench_raw):
-    n0 = re.findall('<title>(.*?) - Enchantment', ench_raw[:200], re.S)[0].lower()
-    if 'mana ' in n0 and '5' in n0:
-        mana = re.findall('(mana [^\+]+)', n0)[0]
-        n0 = n0.replace(mana, 'mp5')
-    n1 = re.findall('name_enus":"([^"]+)', ench_raw)[1].lower()
-    return n0, n1
+def get_names(soup: BeautifulSoup):
+    n0 = soup.find("title").text.split(' - ')[0]
+    n1 = soup.find(id="topbar").find_next_sibling().text
+    n1 = re.findall('name_enus":"([^"]+)', n1)[1]
+    return [n0, n1]
 
-def parse_enchant(q):
+def get_value(text):
     try:
-        stat = re.findall("(\d+) ([a-z5\' ]+)", q)[0]
-        return stat[1], int(stat[0])
-    except:
-        print('ERROR PARSING ENCHANT:', q)
-        return '', ''
+        return text and "%" not in text and re.findall("(\d{1,3})", text)[0]
+    except IndexError:
+        return
 
-def get_ench(ench_raw):
-    stats = set()
-    names = get_names(ench_raw)
-    n0 = names[0]
-    if '+' in n0:  
-        if "all stats" in n0:
-            value = int(re.findall('(\d+)', n0)[0])
-            stats |= {(stat, value) for stat in BASE_STATS}
-        else:
-            for stat in n0.split(' and '):
-                if stat[1].isdigit() and '%' not in stat:
-                    stats.add(parse_enchant(stat))
-            #     print(x[1], x[1].isdigit(), x, v)
-            # stats |= {parse_enchant(x) for x in n0.split(' and ') if x[1].isdigit()}
-            # print(stats)
-    if "Statistics" in ench_raw:
-        stats_tmp = re.findall("Value: (\d\d?).+?traits..([^']*)", ench_raw, re.S)
-        stats |= {(short_stats[stat], int(value)) for value, stat in stats_tmp if stat in short_stats}
-    if 'Defense:' in ench_raw:
-        armor = re.findall("Defense: .*?Value: (\d{1,3})<", ench_raw)[0]
-        stats.add(('armor', int(armor)))
-    stats = list(stats)
-    return {'names': names, 'stats': stats}
+def parse_each(td: Tag):
+    small = td.find("small")
+    if small is None:
+        return
 
-if __name__ == "__main__":
+    value = small.text
+    stat_tag = small.find_next_sibling()
+    if stat_tag is None:
+        if not td.text or "Defense: (Physical)" not in td.text:
+            return
+        stat = ["Armor"]
+    elif not stat_tag.get("type"):
+        value = td.find('a').text
+        stat = re.findall("[A-z ]+", value)
+    else:
+        stat = re.findall("\['([a-z]+)", stat_tag.text)
+    value = get_value(value)
+    if value and stat:
+        stat = stat[0].lower().replace('increased', '').strip()
+        return SHORT_STATS.get(stat, stat), int(value)
+    
+def get_ench(ench_raw: str):
+    soup = BeautifulSoup(ench_raw, features="html.parser")
+    stats_table = soup.find(id="spelldetails")
+    stats = [
+        parse_each(td)
+        for td in stats_table.find_all("td")
+    ]
+    stats = [x for x in stats if x]
+    names = get_names(soup)
+    for x in names:
+        if "all stats" not in x.lower(): continue
+        for s in BASE_STATS:
+            stats.append((s, int(get_value(x))))
+    return {
+        "names": names,
+        "stats": stats
+    }
+
+class EnchGetter(threading.Thread):
+    def __init__(self, ench_ID) -> None:
+        super().__init__()
+        self.ench_ID = ench_ID
+        self.file_name = f"{ENCH_CACHE}/{self.ench_ID}.json"
+
+    def run(self):
+        try:
+            with open(self.file_name, 'r') as f:
+                CACHED[self.ench_ID] = json.load(f)
+        except FileNotFoundError:
+            ench_raw = get_page(self.ench_ID)
+            ench = get_ench(ench_raw)
+            CACHED[self.ench_ID] = ench
+            self.save(ench)
+
+    def save(self, data):
+        with open(self.file_name, 'w') as f:
+            json.dump(data, f)
+
+def main(ench_ID):
+    if ench_ID in CACHED:
+        return CACHED[ench_ID]
+    ench_t = LOADING.get(ench_ID)
+    if ench_t is None:
+        ench_t = EnchGetter(ench_ID)
+        ench_t.start()
+        LOADING[ench_ID] = ench_t
+    ench_t.join()
+    return CACHED[ench_ID]
+
+
+def __test():
     qq = [
         3633, 3628, 3605, 2933, 2938, 3294, 2679, 3789, 3623, 3548, 3590, 3820, 3859,
         3520, 3810, 3832, 3758, 3604, 3520, 3719, 3606, 3560, 3520, 3834, 3520, 3563,
         3545, 3859, 3232, 3747, 3243, 3247, 3722, 2673, 2381, 3546, 3819, 3627, 3244]
     for ench_ID in qq:
-        print(ench_ID)
-        ench_raw = get_ench_raw(ench_ID)
-        ench = get_ench(ench_raw)
+        ench = main(ench_ID)
         print(ench)
+
+if __name__ == "__main__":
+    __test()
